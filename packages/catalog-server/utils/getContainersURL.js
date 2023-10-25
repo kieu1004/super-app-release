11170388c21e3202ba6cd10d3e8cca0b7320ae72
Nguyen Thi Bich Kieu <109791045+kieu1004@users.noreply.github.com>
2023-10-25 10:13:09 +0700

    done bundle

--- packages/catalog-server/utils/getContainersURL.js
+++ packages/catalog-server/utils/getContainersURL.js
@@ -1,6 +1,6 @@
 const getContainersURL = ({
-  // hostname = 'http://192.168.0.4:3000',
-  hostname = 'http://192.168.0.4:3000',
+  // hostname = 'http://192.168.10.239:3000',
+  hostname = 'http://192.168.10.239:3000',
   appName,
   version,
   platform,
